9ad21117aa8ce410b2ea58cb980fef1c47fdc48b
jaqlig <74549994+jaqlig@users.noreply.github.com>
2020-12-03 23:14:47 +0100

    printing codes

--- Codes/huffman_code.py
+++ Codes/huffman_code.py
@@ -17,6 +17,7 @@ def tree(node, binary=""):
     return dict
 
 def huffmanCode(text):
+
     freq = {}
     for i in text:
         if i in freq:
@@ -25,23 +26,26 @@ def huffmanCode(text):
             freq[i] = 1
 
     freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
-    nodes = freq
 
-    while len(nodes) >= 2:
-        (character1, freq1) = nodes[-1]
-        (character2, freq2) = nodes[-2]
-        nodes = nodes[:-2]
+    while len(freq) >= 2:
+        (character1, freq1) = freq[-1]
+        (character2, freq2) = freq[-2]
+        freq = freq[:-2]
         node = Node(character1, character2)
-        nodes.append((node, freq1 + freq2))
+        freq.append((node, freq1 + freq2))
 
-        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+        freq = sorted(freq, key=lambda x: x[1], reverse=True)
 
-    huffman_code = tree(nodes[0][0])
-    return(huffman_code)
+    huffman_code = tree(freq[0][0])
+    return huffman_code
 
-def huffman(text, separator=False):
+def codeInHuffman(text, separator=False):
     
-    huffman_code = huffmanCode(text)    
+    huffman_code = huffmanCode(text)
+
+    for i in sorted(huffman_code.keys()):
+        print("{} => {}".format(i, huffman_code[i]))
+
     coded = ""
 
     for i in text:
@@ -55,11 +59,4 @@ def huffman(text, separator=False):
 
 text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
 print(text)
-print(huffman(text))
-
-print("\n")
-
-text2 = "Lorem ipsum dolor sit amet"
-# Second argument adds space between codes, if True
-print(text2)
-print(huffman(text2, True))
+print(codeInHuffman(text, True))
